a9dba37082d05c6d7e57f4d2b5acfd84f61af4b7
shalodin <shalodin.chepkoech@student.moringaschool.com>
2022-04-18 22:52:09 +0300

    Auto-generated commit [ci skip]

--- src/app/onhover.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OnhoverDirective } from './onhover.directive';
-
-describe('OnhoverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OnhoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  ApiKey:"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"
-};
